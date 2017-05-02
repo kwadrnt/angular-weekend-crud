4f2b753bad782ab5908f83df834beeab10ed5869
kwadrnt <kevinctse@gmail.com>
2017-05-01 17:05:51 -0700

    show and index.html added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log('sanity check working');
+
+angular.module('bookApp', ['ngRoute'])
+       .config(config);
+
+
+////////////
+// ROUTES //
+////////////
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/books/index.html',
+      controller: 'booksIndexController',
+      controllerAs: 'booksIndexCtrl'
+    })
+    .when('/books/:id', {
+      templateUrl: 'templates/books/show.html',
+      controller: 'booksShowController',
+      controllerAs: 'booksShowCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+}
